a047d841c53f99025cd65e68c4c9679ba7cc43e4
Turbobot <turbobot@vercel.com>
2025-06-12 16:39:07 +0100

    feat(create-turbo): apply official-starter transform

--- packages/ui/src/index.ts
+++
@@ -1,7 +0,0 @@
-// components
-export * from "./components/Button";
-export * from "./components/SegmentedControl";
-
-// other stuff
-export * from "./theme/ThemeProvider";
-export * from "./theme/theme";
